36deab20da2f9b04051f6de3b5566336514963ae
Alyana-Vandana <109644160+Alyana-Vandana@users.noreply.github.com>
2024-04-16 15:41:49 +0530

    Create test.r

---
+++ test.r
@@ -0,0 +1,13 @@
+# Create the dataset
+efh <- data.frame(
+  cond = c('A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'B', 'B', 'B', 'B', 'B', 'B', 'B'),
+  test = c(0.71, 0.82, 0.82, 0.76, 0.76, 0.71, 0.71, 0.82, 0.65, 0.53, 0.88, 0.59, 0.76, 0.59, 0.65),
+  msat = c(650, 710, 510, 590, 500, 730, 570, 780, 690, 710, 780, 690, 730, 700, 740)
+)
+
+# Print the dataset
+print(efh)
+
+# Perform t-test
+t_test <- t.test(test ~ cond, data = efh)
+t_test
